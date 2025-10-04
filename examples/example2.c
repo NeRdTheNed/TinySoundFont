fcd41338aedb16ffac1f226ffee9fd5c3a5e259e
Ned Loynd <ned@loynd.com>
2025-10-04 18:35:17 +1000

    Test bspline interpolation

--- examples/example2.c
+++ examples/example2.c
@@ -82,14 +82,21 @@ int main(int argc, char *argv[])
 		ma_sleep(1000);
 
 		ma_mutex_lock(&g_Mutex);
-		g_TinySoundFont->interpolateMode = TSF_INTERP_CUBIC_HERMITE;
+		g_TinySoundFont->interpolateMode = TSF_INTERP_HERMITE_4P;
 		tsf_note_off(g_TinySoundFont, i, Notes[i % 7]);
 		tsf_note_on(g_TinySoundFont, i, Notes[i % 7], 1.0f);
 		ma_mutex_unlock(&g_Mutex);
 		ma_sleep(1000);
 
 		ma_mutex_lock(&g_Mutex);
-		g_TinySoundFont->interpolateMode = TSF_INTERP_CUBIC_LAGRANGE;
+		g_TinySoundFont->interpolateMode = TSF_INTERP_LAGRANGE_4P;
+		tsf_note_off(g_TinySoundFont, i, Notes[i % 7]);
+		tsf_note_on(g_TinySoundFont, i, Notes[i % 7], 1.0f);
+		ma_mutex_unlock(&g_Mutex);
+		ma_sleep(1000);
+
+		ma_mutex_lock(&g_Mutex);
+		g_TinySoundFont->interpolateMode = TSF_INTERP_BSPLINE_4P;
 		tsf_note_off(g_TinySoundFont, i, Notes[i % 7]);
 		tsf_note_on(g_TinySoundFont, i, Notes[i % 7], 1.0f);
 		ma_mutex_unlock(&g_Mutex);
